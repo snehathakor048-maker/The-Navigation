4c168bc53991300f8d573fd57f6ea7b40e71d0a0
snehathakor048-maker <snehathakor048@gmail.com>
2026-02-26 18:48:41 +0530

    Implement hamburger menu toggle and auto-close feature
    Added functionality for hamburger menu toggle and auto-close on link click.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Day 3 Navbar Loaded");
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("navMenu");
+const links = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+    document.body.classList.toggle("no-scroll");
+});
+
+/* AUTO CLOSE */
+links.forEach(link=>{
+link.addEventListener("click", ()=>{
+    navMenu.classList.remove("active");
+    document.body.classList.remove("no-scroll");
+});
+});
