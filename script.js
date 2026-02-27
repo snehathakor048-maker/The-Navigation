d0fda256ecdb726fe5f4f399ede660f9d449908e
snehathakor048-maker <snehathakor048@gmail.com>
2026-02-27 18:03:53 +0530

    Implement search form with validation and alert
    Added search form functionality with validation.

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
+
 const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("navMenu");
 const links = document.querySelectorAll(".nav-link");
 
 hamburger.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
-    document.body.classList.toggle("no-scroll");
+navMenu.classList.toggle("active");
+document.body.classList.toggle("no-scroll");
 });
 
-/* AUTO CLOSE */
 links.forEach(link=>{
 link.addEventListener("click", ()=>{
-    navMenu.classList.remove("active");
-    document.body.classList.remove("no-scroll");
+navMenu.classList.remove("active");
+document.body.classList.remove("no-scroll");
 });
+});
+
+
+
+const form = document.getElementById("searchForm");
+const input = document.getElementById("searchInput");
+
+form.addEventListener("submit",(e)=>{
+e.preventDefault();
+
+if(input.value === ""){
+alert("Please enter a search term");
+}else{
+console.log("Searching for:",input.value);
+}
 });
